da4eb57c626bd11fe06f0fc96064d456bd2797d8
RivkyShachar <rvider@g.jct.ac.il>
2023-12-03 15:22:59 +0200

    validate

--- models/studyRequestModel.js
+++ models/studyRequestModel.js
@@ -18,45 +18,36 @@ const studyRequestSchema = new mongoose.Schema({
 exports.StudyRequestModel = mongoose.model("studyRequests", studyRequestSchema);
 
 exports.validateStudyRequest = (_reqBody) => {
-    const schema = Joi.object({
-        topics: validateTopics(),
-        preferredLanguages: validatePreferredLanguages(),
-        preferredHours: validatePreferredHours(),
-        studyDuration: validateStudyDuration(),
-    }).custom(validateDurationAndStudy());
-    return schema.validate(_reqBody);
-};
+    let schemaJoi = Joi.object({
+        topics: Joi.array().items(Joi.string().min(2).max(100)).min(1).max(999).required(),
+        preferredLanguages: Joi.array().items(Joi.string().min(2).max(100)).min(1).max(999).required(),
+        preferredHours: Joi.object({
+            from: Joi.date().required(),
+            to: Joi.date().required(),
+        }).required().when(Joi.object({
+            from: Joi.date(),
+            to: Joi.date(),
+        }), {
+            then: Joi.object({
+                from: Joi.date().required(),
+                to: Joi.date().required().greater(Joi.ref('from')),
+            }),
+        }),
+        studyDuration: Joi.object({
+            min: Joi.number().required().min(5),
+            max: Joi.number().required().min(Joi.ref('min')),
+        }).required(),
+    }).custom((value, helpers) => {
+        // Calculate the duration in hours
+        const from = new Date(value.preferredHours.from);
+        const to = new Date(value.preferredHours.to);
+        const durationInMinutes = (to - from) / (60 * 1000);
+        // Compare with studyDuration
+        if (durationInMinutes < value.studyDuration.min) {
+            return helpers.error(new Error('Preferred Hours must be greater than or equal to Study Duration'));
+        }
 
-function validateTopics() {
-    return Joi.array().items(Joi.string().min(2).max(100)).min(1).max(999).required();
-}
-
-function validatePreferredLanguages() {
-    return Joi.array().items(Joi.string().min(2).max(100)).min(1).max(999).required();
-}
-
-function validatePreferredHours() {
-    return Joi.object({
-        from: Joi.date().required(),
-        to: Joi.date().required().greater(Joi.ref('from')),
-    }).required();
-}
-
-function validateStudyDuration() {
-    return Joi.object({
-        min: Joi.number().required().min(5),
-        max: Joi.number().required().min(Joi.ref('min')),
-    }).required();
-}
-
-function validateDurationAndStudy(value, helpers) {
-    const from = new Date(value.preferredHours.from);
-    const to = new Date(value.preferredHours.to);
-    const durationInHours = (to - from) / (60 * 60 * 1000);
-
-    if (durationInHours < value.studyDuration.min) {
-        return helpers.error(new Error('Preferred Hours must be greater than or equal to Study Duration'));
-    }
-
-    return value;
+        return value;
+    });
+    return schemaJoi.validate(_reqBody);
 }

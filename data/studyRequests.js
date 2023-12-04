2ab9812973e25352280be23e5a64e2cd6a8e1c42
RivkyShachar <rvider@g.jct.ac.il>
2023-12-04 12:09:28 +0200

    add data to put in database

---
+++ data/studyRequests.js
@@ -0,0 +1,243 @@
+[
+    {
+      "preferredHours": {
+        "from": "2023-12-02T08:00:00.000Z",
+        "to": "2023-12-02T10:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 10,
+        "max": 20
+      },
+      "topics": ["Jewish History", "Hebrew Literature"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-02T09:30:00.000Z",
+        "to": "2023-12-02T11:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 15,
+        "max": 25
+      },
+      "topics": ["Judaism", "English Literature"],
+      "preferredLanguages": ["English"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-03T10:00:00.000Z",
+        "to": "2023-12-03T12:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 12,
+        "max": 22
+      },
+      "topics": ["Biblical Studies", "Mathematics"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-03T11:30:00.000Z",
+        "to": "2023-12-03T13:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 18,
+        "max": 28
+      },
+      "topics": ["Kabbalah", "Physics"],
+      "preferredLanguages": ["English"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-04T12:00:00.000Z",
+        "to": "2023-12-04T14:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 14,
+        "max": 24
+      },
+      "topics": ["Talmud", "Chemistry"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-04T13:30:00.000Z",
+        "to": "2023-12-04T15:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 20,
+        "max": 30
+      },
+      "topics": ["Jewish Philosophy", "Computer Science"],
+      "preferredLanguages": ["English"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-05T14:00:00.000Z",
+        "to": "2023-12-05T16:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 16,
+        "max": 26
+      },
+      "topics": ["Halakha", "Statistics"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-05T15:30:00.000Z",
+        "to": "2023-12-05T17:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 22,
+        "max": 32
+      },
+      "topics": ["Jewish Literature", "Economics"],
+      "preferredLanguages": ["English"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-06T16:00:00.000Z",
+        "to": "2023-12-06T18:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 25,
+        "max": 35
+      },
+      "topics": ["Midrash", "Political Science"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-06T17:30:00.000Z",
+        "to": "2023-12-06T19:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 30,
+        "max": 40
+      },
+      "topics": ["Jewish Ethics", "Psychology"],
+      "preferredLanguages": ["English"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-07T18:00:00.000Z",
+        "to": "2023-12-07T20:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 28,
+        "max": 38
+      },
+      "topics": ["Jewish Mysticism", "Sociology"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-07T19:30:00.000Z",
+        "to": "2023-12-07T21:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 32,
+        "max": 42
+      },
+      "topics": ["Tanakh Studies", "History"],
+      "preferredLanguages": ["English"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-08T20:00:00.000Z",
+        "to": "2023-12-08T22:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 35,
+        "max": 45
+      },
+      "topics": ["Rabbinic Literature", "Philosophy"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-08T21:30:00.000Z",
+        "to": "2023-12-08T23:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 40,
+        "max": 50
+      },
+      "topics": ["Jewish Theology", "Geography"],
+      "preferredLanguages": ["English"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-09T22:00:00.000Z",
+        "to": "2023-12-09T00:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 42,
+        "max": 52
+      },
+      "topics": ["Holocaust Studies", "Environmental Science"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-09T23:30:00.000Z",
+        "to": "2023-12-10T01:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 45,
+        "max": 55
+      },
+      "topics": ["Jewish Art", "Anthropology"],
+      "preferredLanguages": ["English"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-10T00:00:00.000Z",
+        "to": "2023-12-10T02:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 48,
+        "max": 58
+      },
+      "topics": ["Yiddish Literature", "Literature"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-10T01:30:00.000Z",
+        "to": "2023-12-10T03:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 50,
+        "max": 60
+      },
+      "topics": ["Jewish Music", "Music"],
+      "preferredLanguages": ["English"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-11T02:00:00.000Z",
+        "to": "2023-12-11T04:00:00.000Z"
+      },
+      "studyDuration": {
+        "min": 52,
+        "max": 62
+      },
+      "topics": ["Hebrew Language", "Linguistics"],
+      "preferredLanguages": ["Hebrew"]
+    },
+    {
+      "preferredHours": {
+        "from": "2023-12-11T03:30:00.000Z",
+        "to": "2023-12-11T05:30:00.000Z"
+      },
+      "studyDuration": {
+        "min": 55,
+        "max": 65
+      },
+      "topics": ["Jewish Film", "Film Studies"],
+      "preferredLanguages": ["English"]
+    }
+  ]
+  
---
+++ data/users.js
@@ -0,0 +1,122 @@
+[
+    {
+        "name": "User 1",
+        "email": "user1@gmail.com",
+        "password": "$2b$10$ECXKpbJ.xztVUp8DGBL6yOf4iJ/kR8Z/pZGvRyLK9v7RFG5ulrNL6",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "User 2",
+        "email": "user2@gmail.com",
+        "password": "$2b$10$JXrNBRgm5twXGsOws/OQr.wRGMkjtN.ZylmgVhXQR2UoU1FjE/FPO",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "User 3",
+        "email": "user3@gmail.com",
+        "password": "$2b$10$fdqFZx7/J.nhGrmn5noE9uA1kJNnQ5f/Y6xi/YKhN0jqTtfqFJtMW",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "User 4",
+        "email": "user4@gmail.com",
+        "password": "$2b$10$ENiSS20ojhdcD5ej15RnLeqGSDxC.1EYKT7jVj2ZHCUVrMyyTV8Iq",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "User 5",
+        "email": "user5@gmail.com",
+        "password": "$2b$10$kkNjiKGoWyib.Pg1d/FIku1Tw8zz0m7v6Y8n/kJKVKn4y3DRqGRXG",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "User 6",
+        "email": "user6@gmail.com",
+        "password": "$2b$10$Ztsw2cm7iB3w82vNqI9E/OFBDDb8X/wS8sFy.9ZalnZh6LP5e.nAe",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "User 7",
+        "email": "user7@gmail.com",
+        "password": "$2b$10$lKt1H5Tq/7xlvWEgzVo2AuS/N8yPBeYPprcVoz/U0gRLKKhqA3zq6",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "User 8",
+        "email": "user8@gmail.com",
+        "password": "$2b$10$zBZz9eqrc2rPFSvsuDKM2eCQ5K78vlzQL7lAhf1Oy1T/zEhbkWv2u",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "User 9",
+        "email": "user9@gmail.com",
+        "password": "$2b$10$y9vvl2w5L9NlWjTO8TstV.fKFWHxtlbZ0ezlMgDKp3r6UL2TLlHj6",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "User 10",
+        "email": "user10@gmail.com",
+        "password": "$2b$10$cRmR5jpbXLNbv8qSvPFOLe2.ZJ1OfwRKYy61Rb7cnC4yokzZlOCde",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "John Doe",
+        "email": "john.doe@gmail.com",
+        "password": "$2b$10$ECXKpbJ.xztVUp8DGBL6yOf4iJ/kR8Z/pZGvRyLK9v7RFG5ulrNL6",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "Alice Johnson",
+        "email": "alice.johnson@gmail.com",
+        "password": "$2b$10$JXrNBRgm5twXGsOws/OQr.wRGMkjtN.ZylmgVhXQR2UoU1FjE/FPO",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "Bob Smith",
+        "email": "bob.smith@gmail.com",
+        "password": "$2b$10$fdqFZx7/J.nhGrmn5noE9uA1kJNnQ5f/Y6xi/YKhN0jqTtfqFJtMW",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "Emily Davis",
+        "email": "emily.davis@gmail.com",
+        "password": "$2b$10$ENiSS20ojhdcD5ej15RnLeqGSDxC.1EYKT7jVj2ZHCUVrMyyTV8Iq",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "Michael White",
+        "email": "michael.white@gmail.com",
+        "password": "$2b$10$kkNjiKGoWyib.Pg1d/FIku1Tw8zz0m7v6Y8n/kJKVKn4y3DRqGRXG",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "Sara Miller",
+        "email": "sara.miller@gmail.com",
+        "password": "$2b$10$Ztsw2cm7iB3w82vNqI9E/OFBDDb8X/wS8sFy.9ZalnZh6LP5e.nAe",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "Daniel Brown",
+        "email": "daniel.brown@gmail.com",
+        "password": "$2b$10$lKt1H5Tq/7xlvWEgzVo2AuS/N8yPBeYPprcVoz/U0gRLKKhqA3zq6",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "Olivia Garcia",
+        "email": "olivia.garcia@gmail.com",
+        "password": "$2b$10$zBZz9eqrc2rPFSvsuDKM2eCQ5K78vlzQL7lAhf1Oy1T/zEhbkWv2u",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "Matthew Taylor",
+        "email": "matthew.taylor@gmail.com",
+        "password": "$2b$10$y9vvl2w5L9NlWjTO8TstV.fKFWHxtlbZ0ezlMgDKp3r6UL2TLlHj6",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    },
+    {
+        "name": "Sophia Martinez",
+        "email": "sophia.martinez@gmail.com",
+        "password": "$2b$10$cRmR5jpbXLNbv8qSvPFOLe2.ZJ1OfwRKYy61Rb7cnC4yokzZlOCde",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+    }
+]
